d81d764552dd1d37b8568a402021961387a24df4
Aurelijus Lukšas <aurelijus.luksas@mif.stud.vu.lt>
2024-09-17 19:09:37 +0300

    Added return button to mode1

--- client/src/boards/main.board.tsx
+++ client/src/boards/main.board.tsx
@@ -1,6 +1,7 @@
 import "./main.board.css";
 import React, {useState} from "react";
 import ModeButton1 from "../components/buttons/modeButton1";
+import ReturnButton from "../components/buttons/returnButton";
 import { createBoard } from "@wixc3/react-board";
 
 
@@ -13,14 +14,22 @@ export default createBoard({
         <button className="MainBoard_userButton" id="userButton"></button>
       </div>
       <div className="MainBoard_content" id="contentDiv">
-        <div className="MainBoard_mode1" id="mode1Div"/>
+        <div className="MainBoard_mode1" id="mode1Div">
+          <ReturnButton label="Return" className="MainBoard_returnButton" onClick={()=>{
+            console.log("Return clicked")
+            const mode1 = document.getElementById("mode1Div") as HTMLDivElement;
+            const selection = document.getElementById("selectionDiv") as HTMLDivElement;
+            mode1.style.display = "none";
+            selection.style.display = "flex";
+          }}/>
+        </div>
         <div className="MainBoard_selection" id="selectionDiv">
           <div className="MainBoard_grid" id="selectionGrid">
             <ModeButton1 label= "Mode 1" className= "MainBoard_gridButton" onClick={()=>{
               console.log("Mode 1 clicked")
               const mode1 = document.getElementById("mode1Div") as HTMLDivElement;
               const selection = document.getElementById("selectionDiv") as HTMLDivElement;
-              mode1.style.zIndex = "12";
+              mode1.style.display = "flex";
               selection.style.display = "none";
             }}/>
             <button className="MainBoard_mode2Button MainBoard_gridButton">
