4df77e8d2cf56db9cd3adaf1c8ed54708610479b
Rokas Baliutavičius <baliutaviciusrokas@gmail.com>
2024-09-17 17:10:33 +0300

    Updated class names

--- client/src/_codux/boards/main.board.tsx
+++ client/src/_codux/boards/main.board.tsx
@@ -1,27 +1,27 @@
 import "./main.board.css";
-import React from "react";
+// import React from "react";
 import { createBoard } from "@wixc3/react-board";
 
 export default createBoard({
   name: "Main",
   Board: () => (
-    <div className="MainBoard_div1">
-      <div className="MainBoard_div2">
-        <button className="MainBoard_button2"></button>
+    <div className="MainBoard_main">
+      <div className="MainBoard_header">
+        <button className="MainBoard_userButton"></button>
       </div>
-      <div className="MainBoard_div4">
-        <div className="MainBoard_div6" />
-        <div className="MainBoard_div5" />
-        <div className="MainBoard_div3">
+      <div className="MainBoard_content">
+        <div className="MainBoard_mode1" />
+        <div className="MainBoard_selection">
           <div className="MainBoard_grid">
             <button className="MainBoard_gridButton">Button</button>
             <button className="MainBoard_gridButton">Button</button>
             <button className="MainBoard_gridButton">Button</button>
           </div>
         </div>
+        <div className="MainBoard_mode2" />
       </div>
     </div>
   ),
   isSnippet: true,
-  environmentProps: { windowHeight: 1080, windowWidth: 1941 },
+  environmentProps: { windowWidth: 1920, windowHeight: 1080 },
 });
