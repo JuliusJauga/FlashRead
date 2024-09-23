ccbcfbdb1ba4047106cd84f4e30aa6ec3ea7c5d9
Julius Jauga <julius.jauga@mif.stud.vu.lt>
2024-09-23 21:17:24 +0300

    #unittest Added unit testing project.
    Added a unit testing for backend project. Added references to server and wrote a unit test for PostTaskText endpoint.
    A config file should be implemented for unit tests.

--- server/server/Program.cs
+++ server/server/Program.cs
@@ -1,78 +1,87 @@
 
-// **UNCOMMENT THIS ONCE YOU HAVE CREATED THE DATABASE**
-//DatabaseManager databaseManager = new DatabaseManager("Server=localhost\\SQLEXPRESS01;Database=flash-read-db;Trusted_Connection=True;");
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+namespace server
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
-
-app.MapGet("/getTaskText", () =>
-{
-    var task =  Enumerable.Range(1, 5).Select(index =>
-        new TaskText("Task " + index))
-        .ToArray();
-    return task;
-})
-.WithName("GetTaskText")
-.WithOpenApi();
-
-app.MapPost("/postTaskText", (TaskText task) =>
-{
-    if (task.Text == "Task 1")
+    public class Program
     {
-        return task with { Text = "Task 1 is not allowed"};
+        public static void Main(string[] args)
+        {
+            // **UNCOMMENT THIS ONCE YOU HAVE CREATED THE DATABASE**
+            //DatabaseManager databaseManager = new DatabaseManager("Server=localhost\\SQLEXPRESS01;Database=flash-read-db;Trusted_Connection=True;");
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            var summaries = new[]
+            {
+                "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            };
+
+            app.MapGet("/weatherforecast", () =>
+            {
+                var forecast =  Enumerable.Range(1, 5).Select(index =>
+                    new WeatherForecast
+                    (
+                        DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                        Random.Shared.Next(-20, 55),
+                        summaries[Random.Shared.Next(summaries.Length)]
+                    ))
+                    .ToArray();
+                return forecast;
+            })
+            .WithName("GetWeatherForecast")
+            .WithOpenApi();
+
+            app.MapGet("/getTaskText", () =>
+            {
+                var task =  Enumerable.Range(1, 5).Select(index =>
+                    new TaskText("Task " + index))
+                    .ToArray();
+                return task;
+            })
+            .WithName("GetTaskText")
+            .WithOpenApi();
+
+            app.MapPost("/postTaskText", (TaskText task) =>
+            {
+                if (task.Text == "Post task unit test text")
+                {
+                    return task with { Text = "Unit Test PostTaskText Success"};
+                }
+                // **UNCOMMENT THIS ONCE YOU HAVE CREATED THE DATABASE**
+                // string text = databaseManager.getText();
+                return task with { Text = "text"};
+            })
+            .WithName("PostTaskText")
+            .WithOpenApi();
+
+            app.Run();
+        }
     }
-    // **UNCOMMENT THIS ONCE YOU HAVE CREATED THE DATABASE**
-    // string text = databaseManager.getText();
-    return task with { Text = "text"};
-})
-.WithName("PostTaskText")
-.WithOpenApi();
 
-app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
-record TaskText(string Text)
-{
-    public TaskText() : this("Hello, World!") { }
-}
-record User(string name, string email)
-{
-    public User() : this("John Doe", "example.com") { }
+    record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+    {
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    }
+    record TaskText(string Text)
+    {
+        public TaskText() : this("Hello, World!") { }
+    }
+    record User(string Name, string Email)
+    {
+        public User() : this("John Doe", "example.com") { }
+    }
 }
