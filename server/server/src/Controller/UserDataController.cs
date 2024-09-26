2e50bc91a71d7a7c318b5a7a4cd2c06f2794abe5
Julius Jauga <julius.jauga@mif.stud.vu.lt>
2024-09-26 13:03:58 +0300

    Restore user reg.

--- server/server/src/Controller/UserDataController.cs
+++ server/server/src/Controller/UserDataController.cs
@@ -5,6 +5,11 @@ namespace server.Controller {
 
     [ApiController]
     public class UserDataController : ControllerBase {
+        private readonly IUserHandler _userHandler;
+        public UserDataController(IUserHandler userHandler)
+        {
+            _userHandler = userHandler;
+        }
         [HttpPost("GetTask")]
         public ITaskResponse PostGetTask(TaskRequest req) {
             ITask task = req.TaskId switch
@@ -13,6 +18,22 @@ namespace server.Controller {
                 _ => throw new System.Exception("Task not found"),// TODO: throw custom exception
             };
             return task.GetResponse(req);
+        }
+        [HttpPost("PostUser")]
+        [RequireHttps]
+        [HttpPost]
+        public async Task<IActionResult> PostUser([FromBody] User user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest("Invalid user data.");
+            }
+            var result = await _userHandler.RegisterUserAsync(user);
+            if (result)
+            {
+                return Ok("User added successfully.");
+            }
+            return StatusCode(500, "An error occurred while adding the user.");
         }
     }
 }
