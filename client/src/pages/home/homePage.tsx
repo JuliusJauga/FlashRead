51ce19947f3b3dcacffe2bfef06c854716e321d7
tikis23 <edvis01x@gmail.com>
2024-10-01 01:34:43 +0300

    Improved main page

--- client/src/pages/home/homePage.tsx
+++ client/src/pages/home/homePage.tsx
@@ -6,14 +6,18 @@ const HomePage: React.FC = () => {
     const navigate = useNavigate();
 
     return (
-        <div className="flex h-screen bg-gray-300">
-            <div className="absolute top-0 right-0">
-                <OptionsButton />
-            </div>
+        <div className="flex flex-col h-screen bg-gray-300">
+            <h1 className="
+                absolute top-1/4 left-1/2 -translate-x-1/2 -translate-y-1/2
+                text-9xl"
+            >FlashRead</h1>
             <div className="m-auto space-x-16 flex items-center">
                 <ModeButton label="Mode 1" onClick={() => navigate('/mode1')} />
                 <ModeButton label="Mode 2" onClick={() => navigate('/mode2')} />
                 <ModeButton label="Mode 3" onClick={() => navigate('/mode3')} />
+            </div>
+            <div className="absolute top-0 right-0">
+                <OptionsButton />
             </div>
         </div>
     );
--- client/src/pages/home/optionsButton.tsx
+++ client/src/pages/home/optionsButton.tsx
@@ -1,42 +1,63 @@
-import { Menu, MenuItem, Button } from '@mui/material';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { Menu } from '@mui/material';
+import { Button } from '@mui/base';
 
 const OptionsButton = () => {
   const navigate = useNavigate();
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
   const open = Boolean(anchorEl);
+
   const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
     setAnchorEl(event.currentTarget);
   };
+
+  const buttonClassName = `
+    px-2 rounded-lg text-white font-bold text-xl bg-green-300 border-4 border-green-500
+    hover:scale-110 transition ease-in-out active:brightness-75 flex justify-center items-center
+  `;
+
+  const MenuButton = ({ text, onClick }: { text: string, onClick: () => void }) => (
+    <Button
+      className={`${buttonClassName} my-0.5`}
+      onClick={() => {
+        setAnchorEl(null);
+        onClick();
+      }}
+    >{text}</Button>
+  );
+  
   return (
     <div>
       <Button
+        className={`${buttonClassName} m-1`}
         aria-controls={open ? 'basic-menu' : undefined}
         aria-haspopup="true"
         aria-expanded={open ? 'true' : undefined}
         onClick={handleClick}
       >
-        Settings
+        &#8801;
       </Button>
       <Menu
         id="basic-menu"
         anchorEl={anchorEl}
         open={open}
         onClose={() => setAnchorEl(null)}
-        MenuListProps={{
-          'aria-labelledby': 'basic-button',
+        sx={{
+          '& .MuiPaper-root': {
+            backgroundColor: 'transparent',
+            boxShadow: 'none',
+            overflow: 'visible',
+          },
+          '& .MuiMenu-list': {
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'flex-end',
+          },
         }}
       >
-        <MenuItem onClick={() => {
-          setAnchorEl(null);
-          navigate('/register');
-        }}>Register</MenuItem>
-
-        <MenuItem onClick={() => {
-          setAnchorEl(null);
-          navigate('/login');
-        }}>Login</MenuItem>
+        <MenuButton text="Register" onClick={() => navigate('/register')} />
+        <MenuButton text="Login" onClick={() => navigate('/login')} />
       </Menu>
     </div>
   );
