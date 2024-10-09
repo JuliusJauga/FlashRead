c98bd7e2741715812abf3db7d088a9683b9aa30e
Aurelijus Lukšas <aurelijus.luksas@mif.stud.vu.lt>
2024-10-09 21:13:06 +0300

    added temporary points for presentation

--- client/src/pages/mode2/mode2Page.tsx
+++ client/src/pages/mode2/mode2Page.tsx
@@ -8,16 +8,17 @@ import "../../boards/css/mode2.css";
 
 const Mode2Page: React.FC = () => {
     const navigate = useNavigate();
+    const [points, setPoints] = React.useState<number>(0);
 
     return (
         <div className='Mode1_content'>
             <div className="mode2_centerDiv" id="mode1Div">
                 <div className="points">
                     <p className="pointsText">Points:</p>
-                    <p className="pointsText" id="points">0</p>
+                    <p className="pointsText" id="points">{points}</p>
                 </div>
                 <div className="w-full h-full">
-                    <Mode2Task />
+                    <Mode2Task setPoints={setPoints} />
                 </div>
                 <div className="mode1_lowerDiv" id="buttonDiv">
                     <CustomButton label="Return" className="wideButton" id="MainBoard_returnButton" onClick={() => navigate("/home")}/>
