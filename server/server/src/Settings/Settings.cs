a04227f939c0c6e6f41d5716d041a3885346cab5
Rokas Baliutavičius <baliutaviciusrokas@gmail.com>
2024-10-22 20:01:07 +0300

    Added fonts to DB

--- server/server/src/Settings/Settings.cs
+++ server/server/src/Settings/Settings.cs
@@ -2,7 +2,7 @@ using Microsoft.EntityFrameworkCore;
 using server.src;
 using server.src.Settings;
 namespace server.src.Settings {
-    public class Settings (FlashDbContext _context){
+    public class Settings (FlashDbContext _context) {
         public async Task<DbSettingsTheme> GetSettingsByThemeAsync(string theme)
         {
             var settings = await _context.SettingsThemes.FirstOrDefaultAsync(s => s.Theme == theme);
@@ -12,11 +12,29 @@ namespace server.src.Settings {
             }
             return settings;
         }
+
+        public async Task<DbSettingsFont> GetSettingsByFontAsync(string font)
+        {
+            var settings = await _context.SettingsFonts.FirstOrDefaultAsync(s => s.Font == font);
+            if (settings == null)
+            {
+                throw new Exception("Settings not found");
+            }
+            return settings;
+        }
+
         public async Task<string[]> GetAllThemesAsync()
         {
             var themes = await _context.SettingsThemes.Select(s => s.Theme).ToArrayAsync();
             return themes;
         }
+
+        public async Task<string[]> GetAllFontsAsync()
+        {
+            var fonts = await _context.SettingsFonts.Select(s => s.Font).ToArrayAsync();
+            return fonts;
+        }
+
 
         public async Task UpdateSelectedTheme(string userId, string newTheme) {
             var userSettings = await _context.UserSettings.FirstOrDefaultAsync(s => s.Id == userId);
@@ -24,6 +42,15 @@ namespace server.src.Settings {
                 throw new Exception("User settings not found");
             }
             userSettings.Theme = newTheme;
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateSelectedFont(string userId, string newFont) {
+            var userSettings = await _context.UserSettings.FirstOrDefaultAsync(s => s.Id == userId);
+            if (userSettings == null) {
+                throw new Exception("User settings not found");
+            }
+            userSettings.Font = newFont;
             await _context.SaveChangesAsync();
         }
     }    
