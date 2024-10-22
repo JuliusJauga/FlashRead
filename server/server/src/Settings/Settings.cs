4890aa2c0bea6b06a14b44de6d4de61ff87377a0
Julius Jauga <julius.jauga@mif.stud.vu.lt>
2024-10-22 15:13:22 +0300

    Added getting settings endpoints.

--- server/server/src/Settings/Settings.cs
+++ server/server/src/Settings/Settings.cs
@@ -3,9 +3,19 @@ using server.src;
 using server.src.Settings;
 namespace server.src.Settings {
     public class Settings (FlashDbContext _context){
-        public async Task<string?> GetSettingsByIdAsync(string id)
+        public async Task<DbSettingsTheme> GetSettingsByThemeAsync(string theme)
         {
-            return "not done";
+            var settings = await _context.SettingsThemes.FirstOrDefaultAsync(s => s.Theme == theme);
+            if (settings == null)
+            {
+                throw new Exception("Settings not found");
+            }
+            return settings;
+        }
+        public async Task<string[]> GetAllThemesAsync()
+        {
+            var themes = await _context.SettingsThemes.Select(s => s.Theme).ToArrayAsync();
+            return themes;
         }
     }    
 }
